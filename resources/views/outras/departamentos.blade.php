6daef701a7f9d5d4d6c47cf37985ef5c4dd8dcea
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-11 10:35:36 -0300

    Criando um alias para um componente

--- resources/views/outras/departamentos.blade.php
+++ resources/views/outras/departamentos.blade.php
@@ -13,22 +13,22 @@
         <li>Roupas</li>
     </ul>
 
-    @component('componentes.alerta',['titulo' => 'Erro Fatal', 'tipoAlerta'=>'info'])
+    @alerta(['titulo' => 'Erro Fatal', 'tipoAlerta'=>'info'])
         <p><strong>Erro Inesperado</strong></p>
         <p>Ocorreu um erro inesperado</p>
-    @endcomponent
+    @alerta
 
-    @component('componentes.alerta',['titulo' => 'Erro Fatal', 'tipoAlerta'=>'error'])
+    @alerta(['titulo' => 'Erro Fatal', 'tipoAlerta'=>'error'])
         <p><strong>Erro Inesperado</strong></p>
         <p>Ocorreu um erro inesperado</p>
-    @endcomponent
-    @component('componentes.alerta',['titulo' => 'Erro Fatal', 'tipoAlerta'=>'success'])
+    @endalerta
+    @alerta(['titulo' => 'Erro Fatal', 'tipoAlerta'=>'success'])
         <p><strong>Erro Inesperado</strong></p>
         <p>Ocorreu um erro inesperado</p>
-    @endcomponent
+    @endalerta
 
-    @component('componentes.alerta',['titulo' => 'Erro Fatal', 'tipoAlerta'=>'warning'])
+    @alerta(['titulo' => 'Erro Fatal', 'tipoAlerta'=>'warning'])
         <p><strong>Erro Inesperado</strong></p>
         <p>Ocorreu um erro inesperado</p>
-    @endcomponent
+    @endalerta
 @endsection
